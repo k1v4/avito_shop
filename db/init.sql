9f740b61610eb5cdff9d0e1ff4900c434ef67b89
Kirill Ivanchin <chvik1@yandex.ru>
2025-02-16 02:53:45 +0300

    update init file fo DB

--- db/init.sql
+++ db/init.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
                        amount INT DEFAULT 1000
 );
 CREATE INDEX IF NOT EXISTS idx_username ON users (username);
+CREATE INDEX IF NOT EXISTS idx_id ON users (id);
 
 -- Создание таблицы Items
 CREATE TABLE items (
@@ -13,6 +14,8 @@ CREATE TABLE items (
                        name VARCHAR(255) NOT NULL,
                        price INT NOT NULL
 );
+CREATE INDEX IF NOT EXISTS idx_items_id ON items (id);
+CREATE INDEX IF NOT EXISTS idx_items_name ON items (name);
 
 -- Создание таблицы Inventory
 CREATE TABLE inventory (
@@ -29,13 +32,15 @@ CREATE UNIQUE INDEX idx_user_item ON inventory (user_id, item_id);
 -- Создание таблицы CoinHistory
 CREATE TABLE coin_history (
                              id SERIAL PRIMARY KEY,
-                             --type VARCHAR(50) NOT NULL CHECK (type IN ('received', 'sent')),
                              from_user INT,
                              to_user INT,
                              amount INT NOT NULL,
                              FOREIGN KEY (from_user) REFERENCES users(id),
                              FOREIGN KEY (to_user) REFERENCES users(id)
 );
+CREATE INDEX IF NOT EXISTS idx_from_user_coin_history ON coin_history (from_user);
+CREATE INDEX IF NOT EXISTS idx_to_user_coin_history ON coin_history (to_user);
+
 
 INSERT INTO items(name, price) VALUES ('t-shirt', 80);
 INSERT INTO items(name, price) VALUES ('cup', 20);
@@ -47,20 +52,4 @@ INSERT INTO items(name, price) VALUES ('umbrella', 200);
 INSERT INTO items(name, price) VALUES ('socks', 10);
 INSERT INTO items(name, price) VALUES ('wallet', 50);
 INSERT INTO items(name, price) VALUES ('pink-hoody', 500);
-
--- Триггер и функция для него
--- CREATE OR REPLACE FUNCTION add_user_coins()
---     RETURNS TRIGGER AS $$
--- BEGIN
---     INSERT INTO coins (user_id, amount)
---     VALUES (NEW.id, 1000);
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
---
--- CREATE TRIGGER trigger_add_user_coins
---     AFTER INSERT ON Users
---     FOR EACH ROW
--- EXECUTE FUNCTION add_user_coins();
---
 
